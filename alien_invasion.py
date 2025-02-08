663b8aa08ff5b82086dc1335b28c4facbfb391fc
AnindyaDebnathAni <debnathanindya1137@gmail.com>
2025-02-08 19:38:43 +0600

    BG color

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,5 +34,21 @@ if __name__=='__main__':
             self.clock.tick(60)
 
 
+#BACKGROUND COLOR
+
+    def __init__(self):
+        pygame.display.set_caption("Alien Invasion")
+        self.bg_color=(230,230,230)
+
+    def run_game(self):
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                sys.exit()
+
+
+        self.screen.fill(self.bg_color)
+        pygame.display.flip()
+        self.clock.tick(60)
+
 
 
