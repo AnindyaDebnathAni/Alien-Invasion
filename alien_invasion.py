c3ae29966ca149aac4932d4e7d331ee90ac6d244
AnindyaDebnathAni <debnathanindya1137@gmail.com>
2025-02-10 00:03:27 +0600

    Implemeted settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,14 @@
 import sys
 import pygame
+from settings import Settings
 
 #CREATING A PYGAME WINDOW
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
-        self.screen=pygame.display.set_mode((1200,800))
+        self.settings=Settings()
+        self.screen=pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
@@ -46,7 +48,7 @@ if __name__=='__main__':
                 sys.exit()
 
 
-        self.screen.fill(self.bg_color)
+        self.screen.fill(self.settings.bg_color)
         pygame.display.flip()
         self.clock.tick(60)
 
